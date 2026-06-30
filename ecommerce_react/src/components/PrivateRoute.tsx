44ffa4be4c265b902da01fa21833376f335968bb
Daniel Mauricio B <mauricio.bueno@utp.edu.co>
2026-06-30 09:20:36 -0500

    feat(private-route): agregar estilo al componente PrivateRoute

--- ecommerce_react/src/components/PrivateRoute.tsx
+++ ecommerce_react/src/components/PrivateRoute.tsx
@@ -1,7 +1,8 @@
 import {Navigate, Outlet} from "react-router-dom";
 import {useAuth} from "../context/AuthContext";
+import styles from './PrivateRoute.module.css';
 
 export default function PrivateRoute(){
     const {token} = useAuth()
-    return token? <Outlet/>: <Navigate to="/login" replace/>
+    return <div className={styles.wrapper}>{token ? <Outlet/> : <Navigate to="/login" replace/>}</div>
 }
